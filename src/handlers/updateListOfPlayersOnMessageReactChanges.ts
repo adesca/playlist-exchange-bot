2d633445f8f67032ba0bcfb09841e7563c9e6807
adesca <adeolutechs@gmail.com>
2023-09-19 19:58:24 -0500

    Handle reactions more resiliently by fetching the message so we aren't limited to cached reacts

--- src/handlers/updateListOfPlayersOnMessageReactChanges.ts
+++ src/handlers/updateListOfPlayersOnMessageReactChanges.ts
@@ -3,10 +3,18 @@ import {getExchangeBySignupMessageIdOrThrow, setExchangePlayers} from "../store/
 
 export async function updateListOfPlayersOnMessageReactChanges(interaction: MessageReaction | PartialMessageReaction) {
 
-
+    console.log('New reaction on exchange with signup id ', interaction.message.id)
     const exchange = await getExchangeBySignupMessageIdOrThrow(interaction.message.id)
+    console.log('aka ', exchange.exchangeName, ' : ', interaction.message.id)
 
-    const usersInExchangePromises = interaction.users.cache
+    const message = await interaction.message.fetch()
+    const users = await message.reactions.cache.get('🎶')?.users.fetch()
+    if (users === undefined) {
+        console.log('No users were found in the message reactions ')
+        return;
+    }
+
+    const usersInExchangePromises = users
         .filter(usersWithReactions => !usersWithReactions.bot)
         .map(async userWithReactions => {
             const user = await interaction.message.guild!.members.fetch(userWithReactions.id)
@@ -20,6 +28,7 @@ export async function updateListOfPlayersOnMessageReactChanges(interaction: Mess
         })
 
     Promise.all(usersInExchangePromises).then(usersInExchange => {
+        console.log('Setting users', usersInExchange, ' in exchange ', exchange.exchangeName)
         setExchangePlayers(exchange.exchangeName, usersInExchange)
     })
 
